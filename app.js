13110fbe2f18f2ab9a8aba6bcb5f248674ae2465
Chris Gibbons <christ487@icloud.com>
2023-08-03 20:55:23 +0100

    add app.use, import errorHandler

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 require("express-async-errors");
 
 const express = require("express");
+const errorHandler = require("./handlers/errorHandlers");
 
 const app = express();
+
+// End of all routes
+
+app.use(errorHandler);
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
