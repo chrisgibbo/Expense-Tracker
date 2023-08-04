2f238fa98174f3ad42401d0f75230bbbf91fc57a
Chris Gibbons <christ487@icloud.com>
2023-08-04 18:53:37 +0100

    added post to routes

--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const register = require("./controllers/register");
 
 const userRoutes = express.Router();
 
 // routes ....
+
+userRoutes.post("/register", register);
 
 module.exports = userRoutes;
