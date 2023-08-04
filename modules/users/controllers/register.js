59962ff7ec7415e1ef7090168c2ce3a04f070a3c
Chris Gibbons <christ487@icloud.com>
2023-08-04 19:09:59 +0100

    add mongoose, userModel + requireed body

--- modules/users/controllers/register.js
+++ modules/users/controllers/register.js
@@ -1,4 +1,10 @@
+const mongoose = require("mongoose");
+
 const register = (req, res) => {
+  const usersModel = mongoose.model("users");
+
+  const { email, password, confirm_password } = req.body;
+
   res.status(200).json({
     status: "Hello from the server",
   });
