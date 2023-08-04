a7c59fb03b0449cebcd66e872e9a0b48b7577370
Chris Gibbons <christ487@icloud.com>
2023-08-04 19:11:31 +0100

    add async, await, usersModel inputs

--- modules/users/controllers/register.js
+++ modules/users/controllers/register.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
 
-const register = (req, res) => {
+const register = async (req, res) => {
   const usersModel = mongoose.model("users");
 
-  const { email, password, confirm_password } = req.body;
+  const { email, password, confirm_password, name, balance } = req.body;
+
+  await usersModel.create({
+    name: name,
+    email: email,
+    password: password,
+    balance: balance,
+  });
 
   res.status(200).json({
     status: "Hello from the server",
