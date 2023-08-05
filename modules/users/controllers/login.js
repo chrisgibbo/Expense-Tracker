3fb23bcb0c6f24903523ac5275fe81d2bd2ad74a
Chris Gibbons <christ487@icloud.com>
2023-08-05 18:29:38 +0100

    email password validation

--- modules/users/controllers/login.js
+++ modules/users/controllers/login.js
@@ -1,7 +1,9 @@
 const mongooose = require("mongoose");
 
-const login = () => {
+const login = (req, res) => {
   const usersModel = mongoose.model("users");
+
+  const { email, password } = req.body;
 };
 
 module.exports = login;
