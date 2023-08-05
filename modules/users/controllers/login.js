03b5e0991ff7f70d7aceb468fdc57fc4f9acc579
Chris Gibbons <christ487@icloud.com>
2023-08-05 18:36:22 +0100

    correct mongoose spelling, add status success

--- modules/users/controllers/login.js
+++ modules/users/controllers/login.js
@@ -1,9 +1,14 @@
-const mongooose = require("mongoose");
+const mongoose = require("mongoose");
 
 const login = (req, res) => {
   const usersModel = mongoose.model("users");
 
   const { email, password } = req.body;
+
+  res.status(200).json({
+    status: "success",
+    message: "User logged in successfully",
+  });
 };
 
 module.exports = login;
