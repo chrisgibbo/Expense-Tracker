ed7ea9781cf922b3727ae8f7cb7091fdf5e66b87
Chris Gibbons <christ487@icloud.com>
2023-08-06 19:01:01 +0100

    addIncome const to transactionRoutes

--- modules/transactions/transactions.routes.js
+++ modules/transactions/transactions.routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
 const auth = require("../../middleware/auth");
+const addIncome = require("./controllers/addIncome");
 
 const transactionRoutes = express.Router();
 
 userRoutes.use(auth);
+
+transactionRoutes.post(".addIncome", addIncome);
 
 module.exports = transactionRoutes;
