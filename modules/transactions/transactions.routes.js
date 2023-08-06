7ded2b8afa7cd82a9936fa7423f5bdfb4f4cd437
Chris Gibbons <christ487@icloud.com>
2023-08-06 19:19:18 +0100

    correc tpathway on post

--- modules/transactions/transactions.routes.js
+++ modules/transactions/transactions.routes.js
@@ -7,6 +7,6 @@ const transactionRoutes = express.Router();
 
 transactionRoutes.use(auth);
 
-transactionRoutes.post(".addIncome", addIncome);
+transactionRoutes.post("/addIncome", addIncome);
 
 module.exports = transactionRoutes;
