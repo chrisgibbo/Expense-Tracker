f646899d32c8ca6a2b15bb7d083af2b1a2709ba8
Chris Gibbons <christ487@icloud.com>
2023-08-06 16:00:21 +0100

    add mongoose require mongoose

--- modules/users/controllers/userDashboard.js
+++ modules/users/controllers/userDashboard.js
@@ -1,5 +1,9 @@
+const mongoose = require("mongoose");
+
 const userDashboard = (req, res) => {
   console.log(req.user);
+
+  const usersModel = mongoose.model("users");
 
   res.status(200).json({
     status: "Hello from userDashboard!!",
