7ac9175f72d81d8c3ffde80b20af5172f75e54ea
Chris Gibbons <christ487@icloud.com>
2023-08-06 16:18:58 +0100

    add timestamps

--- models/users.model.js
+++ models/users.model.js
@@ -1,28 +1,33 @@
 const mongooose = require("mongoose");
 
-const usersSchema = new mongooose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
-  },
+const usersSchema = new mongooose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
 
-  email: {
-    type: String,
-    required: [true, "email is required"],
-    unique: true,
-  },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+    },
 
-  password: {
-    type: String,
-    required: [true, "password is required"],
-  },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
 
-  balance: {
-    type: Number,
-    required: [true, "balance is required"],
-    default: 0,
+    balance: {
+      type: Number,
+      required: [true, "balance is required"],
+      default: 0,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const usersModel = mongooose.model("users", usersSchema);
 
