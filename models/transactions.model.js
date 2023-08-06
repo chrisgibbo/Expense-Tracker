693c744cc6da336cb35d9ac15a142cbbe8e3c1f5
Chris Gibbons <christ487@icloud.com>
2023-08-06 17:46:10 +0100

    create transaction file, code transactionsSchema with fields

---
+++ models/transactions.model.js
@@ -0,0 +1,29 @@
+const { default: mongoose } = require("mongoose");
+const mongooose = require("mongoose");
+
+const transactionsSchema = new mongooose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.OjectsId,
+      ref: "users",
+      required: true,
+    },
+
+    amount: {
+      type: number,
+      required: true,
+    },
+
+    transactions_type: {
+      type: string,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const transactionsModel = mongooose.model("users", transactionsSchema);
+
+module.exports = transactionsModel;
