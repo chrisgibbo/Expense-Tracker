be2733b081ed06c0ea98fe858809fc0582b4d5c3
Chris Gibbons <christ487@icloud.com>
2023-08-06 10:49:30 +0100

    add accesstoken to try/catch, add req.user

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,10 +3,10 @@ const jsonwebtoken = require("jsonwebtoken");
 const auth = (req, res, next) => {
   //   console.log(req.headers);
 
-  const accessToken = req.headers.authorization.replace("Bearer ", "");
-
   try {
+    const accessToken = req.headers.authorization.replace("Bearer ", "");
     const jwt_payload = jsonwebtoken.verify(accessToken, process.env.jwt_salt);
+    req.user = jwt_payload;
   } catch (e) {
     res.status(401).json({
       status: "failed",
