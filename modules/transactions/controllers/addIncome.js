9484c81248b9cc55b548a35cee40d7118ce2bfb3
Chris Gibbons <christ487@icloud.com>
2023-08-07 20:26:32 +0100

    add mongoose, const usersModel

--- modules/transactions/controllers/addIncome.js
+++ modules/transactions/controllers/addIncome.js
@@ -1,4 +1,8 @@
+const mongoose = require("mongoose");
+
 const addIncome = (req, res) => {
+  const usersModel = mongoose.model("users");
+  const transactionsModel = mongoose.model("transactions");
   res.status(200).json({
     status: "Hello from add Income!",
   });
