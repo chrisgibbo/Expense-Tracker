41e539d8711a5afc07798d8d96e275bc45c91386
Chris Gibbons <christ487@icloud.com>
2023-08-08 15:54:29 +0100

    import addExpense

--- modules/transactions/transactions.routes.js
+++ modules/transactions/transactions.routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const auth = require("../../middleware/auth");
 const addIncome = require("./controllers/addIncome");
+const addExpense = require("./controllers/addExpense");
 
 const transactionRoutes = express.Router();
 
 transactionRoutes.use(auth);
 
 transactionRoutes.post("/addIncome", addIncome);
+transactionRoutes.post("/addExpense", addExpense);
 
 module.exports = transactionRoutes;
