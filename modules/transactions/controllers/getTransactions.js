192d5edbf0ef1fb7f122e02541ea18b8abf7c7d1
Chris Gibbons <christ487@icloud.com>
2023-08-08 17:23:11 +0100

    add mongoose

--- modules/transactions/controllers/getTransactions.js
+++ modules/transactions/controllers/getTransactions.js
@@ -1,4 +1,7 @@
+const mongoose = require("mongoose");
+
 const getTransactions = (req, res) => {
+  const transactionmodel = mongoose.model("transactions");
   res.status(200).json({
     status: "Transactions",
   });
