c9e5ee10d00e384f6c8d015be93ab6e3a4c04870
Chris Gibbons <christ487@icloud.com>
2023-08-22 21:44:06 +0100

    add throw if variables not met, add const and awai/async

--- modules/users/controllers/resetPassword.js
+++ modules/users/controllers/resetPassword.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
 
-const resetPassword = (req, res) => {
+const resetPassword = async (req, res) => {
   const usersModel = mongoose.model("users");
 
   const { email, new_password, reset_code } = req.body;
+
+  if (!email) throw "Email is required";
+  if (!password) throw "Please provide new password!";
+  if (!reset_code) throw "Please provide reset code!";
+  if (!new_password.length < 5)
+    throw "Password must be at least 5 characters long!";
+
+  const getUserWithResetCode = await usersModel.findOne({
+    email: email,
+    reset_code: reset_code,
+  });
 
   res.status(200).json({
     status: "Reset Password!",
