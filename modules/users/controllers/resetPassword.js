0c8a44dab2de4d5b771ad62b2efba18aefef9956
Chris Gibbons <christ487@icloud.com>
2023-08-22 21:38:32 +0100

    add const with req.body

--- modules/users/controllers/resetPassword.js
+++ modules/users/controllers/resetPassword.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 
 const resetPassword = (req, res) => {
   const usersModel = mongoose.model("users");
+
+  const { email, new_password, reset_code } = req.body;
+
   res.status(200).json({
     status: "Reset Password!",
   });
