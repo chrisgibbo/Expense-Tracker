d67abd14c5250165e0e5286efdc3d7978dbe1a54
Chris Gibbons <christ487@icloud.com>
2023-08-22 19:57:08 +0100

    add mongoose

--- modules/users/controllers/forgotPassword.js
+++ modules/users/controllers/forgotPassword.js
@@ -1,4 +1,7 @@
+const mongoose = require("mongoose");
+
 const forgotPassword = (req, res) => {
+  const userModel = mongoose.model("users");
   res.status(200).json({
     status: "Forgot Password",
   });
