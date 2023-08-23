dbd94352f450120dabe94b0c9d92472fbb8eae31
Chris Gibbons <christ487@icloud.com>
2023-08-23 20:39:14 +0100

    add async/await, throw with string

--- modules/transactions/controllers/deleteTransaction.js
+++ modules/transactions/controllers/deleteTransaction.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose");
 
-const deleteTransaction = (req, res) => {
+const deleteTransaction = async (req, res) => {
   const transactionModel = mongoose.model("transactions");
+
+  const { transaction_id } = req.params;
+
+  const getTransaction = await transactionModel.findOne({
+    _id: transaction_id,
+  });
+
+  if (!getTransaction) throw "Transaction not found";
 
   res.status(200).json({
     status: "Deleted Successfully",
