91e6c2654135fa16b9322bf933f8ecfa9ba0b903
Chris Gibbons <christ487@icloud.com>
2023-08-23 20:05:38 +0100

    add code to emailManager

--- managers/emailManager.js
+++ managers/emailManager.js
@@ -1,3 +1,20 @@
-const emailManager = () => {};
+const emailManager = async (to, text, html, subject) => {
+  var transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "0f221d3f69109c",
+      pass: "756af8fb5638e1",
+    },
+  });
+
+  await transport.sendMail({
+    to: to,
+    from: "info@expensetracker.com",
+    text: text,
+    html: html,
+    subject: subject,
+  });
+};
 
 module.exports = emailManager;
