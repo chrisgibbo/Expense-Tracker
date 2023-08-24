eebd7fa2e9808b3a73d79f0c182bb843736f7ca2
Chris Gibbons <christ487@icloud.com>
2023-08-24 20:25:53 +0100

    import validator, copy code from deleteTransaction and add if statements

--- modules/transactions/controllers/editTransaction.js
+++ modules/transactions/controllers/editTransaction.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const editTransaction = (req, res) => {
+const editTransaction = async (req, res) => {
   const transactionModel = mongoose.model("transactions");
 
   const { transaction_id } = req.body;
 
   if (!transaction_id) throw "Transaction id is required";
+
+  if (!validator.isMongoId(transaction_id.toString()))
+    throw "Please provide a valid ID";
+
+  const getTransaction = await transactionModel.findOne({
+    _id: transaction_id,
+  });
+
+  if (!getTransaction) throw "Transaction not found";
 
   res.status(200).json({
     status: "Edit Transaction",
