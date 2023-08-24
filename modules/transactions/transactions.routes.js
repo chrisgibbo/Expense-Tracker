6f92b7ba200b267b4a078ad894df6d2f87cedaab
Chris Gibbons <christ487@icloud.com>
2023-08-24 20:09:49 +0100

    add comments seperating routes

--- modules/transactions/transactions.routes.js
+++ modules/transactions/transactions.routes.js
@@ -8,11 +8,21 @@ const deleteTransaction = require("./controllers/deleteTransaction");
 
 const transactionRoutes = express.Router();
 
+// Use Routes
+
 transactionRoutes.use(auth);
+
+//  Post Routes
 
 transactionRoutes.post("/addIncome", addIncome);
 transactionRoutes.post("/addExpense", addExpense);
+
+// Get Routes
+
 transactionRoutes.get("/", getTransactions);
+
+// Delete Routes
+
 transactionRoutes.delete("/:transaction_id", deleteTransaction);
 
 module.exports = transactionRoutes;
