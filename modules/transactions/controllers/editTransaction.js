76e9173046512300f70a67522d0c006827230a3f
Chris Gibbons <christ487@icloud.com>
2023-08-24 20:21:06 +0100

    add transactionModel, if state with throw

--- modules/transactions/controllers/editTransaction.js
+++ modules/transactions/controllers/editTransaction.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 
 const editTransaction = (req, res) => {
+  const transactionModel = mongoose.model("transactions");
+
+  const { transaction_id } = req.body;
+
+  if (!transaction_id) throw "Transaction id is required";
+
   res.status(200).json({
     status: "Edit Transaction",
   });
