a844aa331d5bd4f004f5b7387bb47a945aa7ab5b
Chris Gibbons <christ487@icloud.com>
2023-08-24 20:10:01 +0100

    create new file editTransactions

---
+++ modules/transactions/controllers/editTransaction.js
@@ -0,0 +1,9 @@
+// const mongoose = require("mongoose");
+
+const editTransaction = (req, res) => {
+  res.status(200).json({
+    status: "Edit Transaction",
+  });
+};
+
+module.exports = editTransaction;
