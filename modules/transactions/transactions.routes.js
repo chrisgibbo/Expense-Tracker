1b9cb21d3bcc214241d3507e50375ceb3603ec6d
Chris Gibbons <christ487@icloud.com>
2023-08-24 20:15:30 +0100

    import editTransaction and patch route

--- modules/transactions/transactions.routes.js
+++ modules/transactions/transactions.routes.js
@@ -5,6 +5,7 @@ const addIncome = require("./controllers/addIncome");
 const addExpense = require("./controllers/addExpense");
 const getTransactions = require("./controllers/getTransactions");
 const deleteTransaction = require("./controllers/deleteTransaction");
+const editTransaction = require("./controllers/editTransaction");
 
 const transactionRoutes = express.Router();
 
@@ -24,5 +25,6 @@ transactionRoutes.get("/", getTransactions);
 // Delete Routes
 
 transactionRoutes.delete("/:transaction_id", deleteTransaction);
+transactionRoutes.patch("/", editTransaction);
 
 module.exports = transactionRoutes;
