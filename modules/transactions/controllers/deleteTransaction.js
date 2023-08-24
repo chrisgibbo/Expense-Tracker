246a64d958ef32f817ff995f65312300fabaddee
Chris Gibbons <christ487@icloud.com>
2023-08-24 19:27:13 +0100

    add validator, string with throw and remove transaction ID

--- modules/transactions/controllers/deleteTransaction.js
+++ modules/transactions/controllers/deleteTransaction.js
@@ -1,15 +1,23 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const deleteTransaction = async (req, res) => {
   const transactionModel = mongoose.model("transactions");
 
   const { transaction_id } = req.params;
 
+  if (!validator.isMongoId(transaction_id.toString()))
+    throw "Please provide a valid ID";
+
   const getTransaction = await transactionModel.findOne({
     _id: transaction_id,
   });
 
   if (!getTransaction) throw "Transaction not found";
+
+  await transactionModel.deleteOne({
+    _id: transaction_id,
+  });
 
   res.status(200).json({
     status: "Deleted Successfully",
